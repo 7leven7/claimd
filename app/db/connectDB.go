4b486c02a7a3e452444bd32ee9b584c976ef93d6
brankokragovic <branko.kragovic@digitalinsurance.io>
2023-02-03 08:05:35 +0100

    removing hardcoded values

--- app/db/connectDB.go
+++ app/db/connectDB.go
@@ -2,30 +2,29 @@ package db
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 // ConnectDB connects to the database
 func ConnectDB(config *Config) error {
-	var err error
-	//err := godotenv.Load("./.env")
-	//if err != nil {
-	//	log.Fatalf("Some error occured. Err: %s", err)
-	//}
-	//
-	//host := os.Getenv("DB_HOST")
-	//user := os.Getenv("DB_USER")
-	//password := os.Getenv("DB_PASSWORD")
-	//dbname := os.Getenv("DB_NAME")
-	//port := os.Getenv("DB_PORT")
-	//
-	//dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Fatalf("Some error occured. Err: %s", err)
+	}
 
-	dsn := fmt.Sprintf("host=postgres user=claimd password=claimdbko dbname=claimd port=5432 sslmode=disable")
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, dbname, port)
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
